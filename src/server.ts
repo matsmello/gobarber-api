80a58140b02dcf784c5c8ffb01715cc78c372f9c
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-20 19:30:31 -0300

    Add some endpoint to test debug on visual studio code

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,22 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "ok" });
 });
+
+app.post("/clients", (request, response) => {
+  let {name, email} = request.body
+
+  let user = {
+    name,
+    email
+  }
+
+  return response.json(user)
+})
 
 app.listen(3333, () => {
   console.log("✌🏻Server started");
