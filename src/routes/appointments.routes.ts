7a664b1d651119f27d84bc7b97f88adeef7a9d60
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-20 20:12:22 -0300

    Add first post route on appointement endpoint

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import {Router, response} from 'express';
+import {uuid} from 'uuidv4';
+import {startOfHour, parseISO, isEqual} from 'date-fns'
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = []
+
+appointmentsRouter.post("/", (request, response) => {
+
+  const {provider, date} = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date))
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: "This appointment is already booked"})
+  }
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date: parsedDate
+  }
+
+  appointments.push(appointment)
+
+  return response.json({ message: "Appointment was booked successfully.", appointment})
+})
+
+export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
 import express from "express";
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "ok" });
-});
-
-app.post("/clients", (request, response) => {
-  let {name, email} = request.body
-
-  let user = {
-    name,
-    email
-  }
-
-  return response.json(user)
-})
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("✌🏻Server started");
