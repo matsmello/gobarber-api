18b0c8ed4aae8f7ba02407f11bfe2b5101ca3347
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-20 21:06:56 -0300

    Use Appointment Model on routes

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,14 +1,9 @@
-import {Router, response} from 'express';
+import {Router} from 'express';
 import {uuid} from 'uuidv4';
 import {startOfHour, parseISO, isEqual} from 'date-fns'
+import Appointment from './../models/Appointment'
 
 const appointmentsRouter = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = []
 
@@ -24,11 +19,7 @@ appointmentsRouter.post("/", (request, response) => {
     return response.status(400).json({ message: "This appointment is already booked"})
   }
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date: parsedDate
-  }
+  const appointment = new Appointment(provider, parsedDate)
 
   appointments.push(appointment)
 
