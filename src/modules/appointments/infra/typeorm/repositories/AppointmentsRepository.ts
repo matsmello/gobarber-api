6d51c527b957c1e9f7981ec17f504783142988ac
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-28 10:15:21 -0300

    Create interface to delegate responsability to who implements it

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,16 +1,29 @@
 import Appointment from "@modules/appointments/infra/typeorm/entities/Appointment";
-import { EntityRepository, Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
+class AppointmentsRepository  implements IAppointmentsRepository{
+  private ormRepository: Repository<Appointment>;
+  
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>  implements IAppointmentsRepository{
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({provider_id, date}: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date })
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
