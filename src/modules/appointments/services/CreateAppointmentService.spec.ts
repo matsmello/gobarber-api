028b8d1d06de00a0d03ef17b7b0e76a9d926ae21
Matheus Mello <matheusmello_22@hotmail.com>
2020-07-30 12:18:32 -0300

    Create appointment test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+
+import AppError from '@shared/errors/AppError'
+import CreateAppointmentService from './CreateAppointmentService'
+import FakeAppointmentRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository'
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+      const fakeAppointmentRepository = new FakeAppointmentRepository();
+      const createAppointment = new CreateAppointmentService(
+        fakeAppointmentRepository
+      );
+
+      const appointment = await createAppointment.execute({
+        date: new Date,
+        provider_id: '123123'
+      })
+
+      expect(appointment).toHaveProperty('id');
+      expect(appointment.provider_id).toBe('123123');
+
+  })
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository
+    );
+
+    const appointmentDate = new Date();
+
+    const appointment = await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123'
+    })
+
+    expect(createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123'
+    })).rejects.toBeInstanceOf(AppError);
+  })
+})
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+
 import express, { NextFunction, Response, Request } from "express";
 import "express-async-errors";
 import cors from "cors";
