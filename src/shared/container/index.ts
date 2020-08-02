9bc289a15f326a36119888c6a9208133aadeb179
Matheus Mello <matheusmello_22@hotmail.com>
2020-08-01 21:58:31 -0300

    Create UserToken

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,13 +1,26 @@
-import {container} from 'tsyringe';
+import { container } from 'tsyringe';
 
-import '@modules/users/providers'
-import '@shared/container/providers'
+import '@modules/users/providers';
+import '@shared/container/providers';
 
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
-import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-import IUsersRepository from '@modules/users/repositories/IUsersRepository'
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-container.registerSingleton<IAppointmentsRepository>('AppointmentRepository', AppointmentRepository)
-container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentRepository',
+  AppointmentRepository,
+);
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
+);
