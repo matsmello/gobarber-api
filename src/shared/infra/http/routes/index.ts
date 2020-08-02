45f9e3368dc36fcb7293ce54f034375b299ce3d9
Matheus Mello <matheusmello_22@hotmail.com>
2020-08-02 16:55:37 -0300

    Create Update/Show Profile endpoint

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,14 +1,16 @@
-import { Router } from "express";
-import appointments from "@modules/appointments/infra/http/routes/appointments.routes";
-import users from "@modules/users/infra/http/routes/users.routes";
-import sessions from "@modules/users/infra/http/routes/sessions.routes";
-import passwords from "@modules/users/infra/http/routes/password.routes";
+import { Router } from 'express';
+import appointments from '@modules/appointments/infra/http/routes/appointments.routes';
+import users from '@modules/users/infra/http/routes/users.routes';
+import sessions from '@modules/users/infra/http/routes/sessions.routes';
+import password from '@modules/users/infra/http/routes/password.routes';
+import profile from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
-routes.use("/appointments", appointments);
-routes.use("/users", users);
-routes.use("/sessions", sessions);
-routes.use("/password", passwords);
+routes.use('/appointments', appointments);
+routes.use('/users', users);
+routes.use('/sessions', sessions);
+routes.use('/password', password);
+routes.use('/profile', profile);
 
 export default routes;
